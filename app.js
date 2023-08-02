196a3775cb713beda18fe376ead7006a01d16971
Kirill Malskiy <sabbat@mail.ru>
2023-08-02 07:32:18 +0300

    подключился к Mongo

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = 3000;
+
+mongoose
+  .connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('подключение к бд');
+  });
 
 const app = express();
 
