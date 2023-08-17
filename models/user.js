85d214c7a93bd4ec79d973b34e0deebc7fac6356
Kirill Malskiy <sabbat@mail.ru>
2023-08-17 15:02:02 +0300

    добавил email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: validator.isEmail,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
-
   },
   about: {
     type: String,
